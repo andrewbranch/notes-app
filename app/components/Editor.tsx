13409312a068a918b0283dc23ccc0195461107aa
Andrew Branch <andrew@wheream.io>
2018-03-03 13:03:20 -0800

    Apply entities to backtick ranges

--- app/components/Editor.tsx
+++ app/components/Editor.tsx
@@ -6,47 +6,96 @@ import { default as DraftEditor, Plugin } from 'draft-js-plugins-editor';
 import { connect } from 'react-redux';
 import { editorSelector } from './Editor.selectors';
 import * as editorActions from './Editor.actions';
+import { stripEntitiesFromBlock } from '../utils/draft-utils';
 
 const stylingEntities = [{
   name: 'inlineCode',
   rawPattern: /`([^`]+)`/g,
   format: (matchArray: RegExpMatchArray) => matchArray[1],
   createEntity: (currentContent: ContentState) => currentContent.createEntity(
-    'core.inlineCode',
+    'core.styling.inlineCode',
     'MUTABLE'
   )
-}]
+}];
+
+const processChange = (editorState: EditorState, insertedCharacter: string | null, isBackspace = false): EditorState => {
+  const selectionState = editorState.getSelection()
+  const cursorPositionKey = selectionState.getStartKey();
+  let contentState = editorState.getCurrentContent();
+
+  // Because a single character change could change all entities in a block,
+  // easiest thing to do is to delete them all and recreate them all.
+  // This probably isn’t the most efficient thing, so we might need to
+  // revisit this logic later if performance suffers.
+  contentState = stripEntitiesFromBlock(
+    contentState,
+    cursorPositionKey,
+    entity => entity.getType().startsWith('core.styling')
+  );
+
+  // Insert character manually:
+  // If we’re inserting a character in `handleBeforeInput`, the text hasn’t yet been updated,
+  // and we need to do it ourselves. In the case of `isBackspace`, the content has already
+  // been updated in the `editorState` we were passed, so no need to do anything.
+  if (insertedCharacter) {
+    contentState = Modifier.insertText(contentState, selectionState, insertedCharacter);
+  }
+
+  const newText = contentState.getBlockForKey(cursorPositionKey).getText();
+
+  // Go through each styling entity and reapply
+  stylingEntities.forEach(style => {
+    let matchArr;
+    do {
+      matchArr = style.rawPattern.exec(newText);
+      if (matchArr) {
+        contentState = style.createEntity(contentState);
+        const entityKey = contentState.getLastCreatedEntityKey();
+        const entitySelection = selectionState.merge({
+          anchorOffset: matchArr.index,
+          focusOffset: matchArr.index + matchArr[0].length
+        }) as SelectionState;
+        contentState = Modifier.applyEntity(contentState, entitySelection, entityKey);
+      }
+    } while (matchArr);
+  });
+
+  let newEditorState = EditorState.push(editorState, contentState, 'apply-entity');
+  if (newEditorState !== editorState) {
+    // If we manually inserted a character, we need to move the selection forward by one character.
+    if (insertedCharacter) {
+      const newCursorPosition = selectionState.getAnchorOffset() + 1;
+      newEditorState = EditorState.forceSelection(newEditorState, selectionState.merge({
+        anchorOffset: newCursorPosition,
+        focusOffset: newCursorPosition
+      }) as SelectionState);
+    } else {
+      // If we were processing a backspace, we just need to put the selection state back how it was
+      // before applying the entity.
+      newEditorState = EditorState.forceSelection(newEditorState, selectionState);
+    }
+  }
+
+  return newEditorState;
+}
 
 const plugin: Plugin = {
   handleBeforeInput: (character, editorState, pluginProvider) => {
-    const selectionState = editorState.getSelection()
-    const cursorPositionKey = selectionState.getStartKey();
-    const text = editorState.getCurrentContent().getBlockForKey(cursorPositionKey).getText();
-    const newText = text + character;
-    let contentState = editorState.getCurrentContent();
-    stylingEntities.forEach(style => {
-      let matchArr;
-      do {
-        matchArr = style.rawPattern.exec(newText);
-        if (matchArr) {
-          contentState = style.createEntity(contentState);
-          const entityKey = contentState.getLastCreatedEntityKey();
-          const entitySelection = selectionState.merge({
-            anchorOffset: matchArr.index,
-            focusOffset: matchArr.index + matchArr[0].length - 1
-          }) as SelectionState;
-          contentState = Modifier.applyEntity(contentState, entitySelection, entityKey);
-        }
-      } while (matchArr);
-    });
-
-    const newEditorState = EditorState.push(editorState, contentState, 'apply-entity');
-    if (newEditorState !== editorState) {
+    const newEditorState = processChange(editorState, character);
+    if (editorState !== newEditorState) {
       pluginProvider.setEditorState(newEditorState);
       return 'handled';
     }
 
     return 'not-handled';
+  },
+
+  onChange: editorState => {
+    if (editorState.getLastChangeType() === 'backspace-character') {
+      return processChange(editorState, null, true);
+    }
+
+    return editorState;
   }
 }
 
---
+++ app/utils/draft-utils.ts
@@ -0,0 +1,36 @@
+import { SelectionState, ContentBlock, Entity, ContentState, Modifier } from 'draft-js';
+
+// Can be replaced with ReturnType<T> in TS 2.8
+try { var _ = Entity.mergeData('', {}); } catch {}
+type EntityInstance = typeof _;
+
+export const createSelectionWithRange = (blockOrKey: ContentBlock | string, start: number, end: number): SelectionState => {
+  const blockKey = typeof blockOrKey === 'string' ? blockOrKey : blockOrKey.getKey();
+  return SelectionState.createEmpty(blockKey).merge({ anchorOffset: start, focusOffset: end }) as SelectionState
+};
+
+export const createSelectionWithSelection = (selectionState: SelectionState, moveStart: number, moveEnd: number): SelectionState => {
+  return selectionState.merge({
+    anchorOffset: selectionState.getAnchorOffset() + moveStart,
+    focusOffset: selectionState.getFocusOffset() + moveEnd
+  }) as SelectionState;
+};
+
+export const createSelectionWithBlock = (block: ContentBlock): SelectionState => (
+  SelectionState.createEmpty(block.getKey()).merge({ focusOffset: block.getLength() }) as SelectionState
+);
+
+export const stripEntitiesFromBlock = (contentState: ContentState, blockOrKey: ContentBlock | string, entityFilter: (entity: EntityInstance) => boolean): ContentState => {
+  const block = typeof blockOrKey === 'string' ? contentState.getBlockForKey(blockOrKey): blockOrKey;
+  let newContentState = contentState;
+  block.findEntityRanges(value => {
+    const entityKey = value.getEntity();
+    const entity = entityKey && contentState.getEntity(entityKey);
+    return entity && entityFilter(entity) || false;
+  }, (start, end) => {
+    const entitySelection = createSelectionWithRange(block, start, end);
+    newContentState = Modifier.applyEntity(newContentState, entitySelection, null);
+  });
+
+  return newContentState;
+}
