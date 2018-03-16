3b8733f6096131951f51b3a496c0fe4ffc8eaaf8
Andrew Branch <andrew@wheream.io>
2018-03-16 12:59:56 -0700

    Reset inline style at the beginning of new blocks

--- app/components/editor/core-styling-plugin/index.ts
+++ app/components/editor/core-styling-plugin/index.ts
@@ -1,12 +1,37 @@
-import { EditorState } from 'draft-js';
+import { EditorState, Modifier } from 'draft-js';
 import { Plugin } from 'draft-js-plugins-editor';
 import { decorators } from './decorators';
 import { updateInlineStyles } from './steps/updateInlineStyles';
 import { styleValues } from './styles';
 import { collapseInlineStyles } from './steps/collapseInlineStyle';
 import { expandInlineStyle } from './steps/expandInlineStyle';
+import { createSelectionWithRange, createSelectionWithSelection } from '../../../utils/draft-utils';
 
 export const createCoreStylingPlugin: (getEditorState: () => EditorState) => Plugin = getEditorState => ({
+  handleBeforeInput: (chars, editorState, { setEditorState }) => {
+    const selection = editorState.getSelection();
+    // Reset inline style in new blocks
+    if (
+      selection.getStartOffset() === 0 &&
+      !editorState.getInlineStyleOverride() &&
+      !editorState.getCurrentInlineStyle().isEmpty()
+    ) {
+      const newSelection = createSelectionWithRange(selection.getStartKey(), 0, 0);
+      setEditorState(EditorState.forceSelection(
+        EditorState.push(
+          editorState,
+          Modifier.insertText(editorState.getCurrentContent(), newSelection, chars),
+          'insert-characters'
+        ),
+        createSelectionWithSelection(newSelection, chars.length, chars.length)
+      ));
+
+      return 'handled';
+    }
+
+    return 'not-handled';
+  },
+
   onChange: editorState => {
     const prevEditorState = getEditorState();
     const editorStateWithNewStyles = updateInlineStyles(editorState, prevEditorState);
