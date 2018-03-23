241c0c6b2b466eda2a8a6d360b8611925137e28b
Andrew Branch <andrew@wheream.io>
2018-03-22 21:53:02 -0700

    Make keybourd shortcuts work while editing notes

--- app/keybindings.ts
+++ app/keybindings.ts
@@ -1,12 +1,13 @@
 import * as Mousetrap from 'mousetrap';
-import { Dispatch, ActionCreator } from 'react-redux';
+import { ActionCreator } from 'react-redux';
 import { isMacOS } from './utils/platform';
 import { createNote } from './actions/notes';
 import { StateShape } from './reducers';
 import { Store } from 'redux';
 
+// @ts-ignore: Mousetrap bindGlobal extension https://github.com/ccampbell/mousetrap/blob/master/plugins/global-bind/mousetrap-global-bind.js
+!function(t){var o={},a=t.prototype.stopCallback;t.prototype.stopCallback=function(t,n,r,i){var e=this;return!!e.paused||!o[r]&&!o[i]&&a.call(e,t,n,r)},t.prototype.bindGlobal=function(t,a,n){if(this.bind(t,a,n),t instanceof Array)for(var r=0;r<t.length;r++)o[t[r]]=!0;else o[t]=!0},t.init()}(Mousetrap);
 const meta = (key: string) => `${isMacOS ? 'command' : 'ctrl'}+${key}`;
-
 
 export type Keybinding<T> = {
   title: string;
@@ -29,7 +30,7 @@ export const keybindings: Keybinding<any>[] = [
 
 export const attachKeyboardHandlers = ({ dispatch, getState }: Store<StateShape>) => {
   keybindings.forEach(keybinding => {
-    Mousetrap.bind(meta('n'), event => {
+    Mousetrap.bindGlobal(meta('n'), event => {
       dispatch(keybinding.action(keybinding.createPayload ? keybinding.createPayload(getState(), event) : undefined));
     });
   });
