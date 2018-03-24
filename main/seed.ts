6be96450da62ac0ad0b02f3299d4c11668c7085d
Andrew Branch <andrew@wheream.io>
2018-03-24 00:44:05 -0700

    Change seed to empty

--- main/seed.ts
+++ main/seed.ts
@@ -1,7 +1,7 @@
 import * as RxDB from 'rxdb';
 import { DBNote } from '../interprocess/types';
 import { RawDraftContentState } from 'draft-js';
-const sampleNoteContent = {"blocks":[{"key":"3cq7l","text":"Let’s try saving this document note to a database.","type":"unstyled","depth":0,"inlineStyleRanges":[{"offset":22,"length":8,"style":"core.styling.strikethrough"},{"offset":41,"length":8,"style":"core.styling.italic"}],"entityRanges":[],"data":{}},{"key":"1tmgr","text":"Neat-o!","type":"unstyled","depth":0,"inlineStyleRanges":[{"offset":0,"length":7,"style":"core.styling.bold"}],"entityRanges":[],"data":{}},{"key":"89ujc","text":"","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}};
+const sampleNoteContent = {"blocks":[{"key":"3cq7l","text":"","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}};
 
 export async function seedNotes(notes: RxDB.RxCollection<DBNote>): Promise<void> {
   if (!await notes.findOne({ id: 'seed-1' }).exec()) {
