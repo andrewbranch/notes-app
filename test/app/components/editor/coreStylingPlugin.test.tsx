25b3a4125ec02c0fa3365b320cfb9b6034966075
Andrew Branch <andrew@wheream.io>
2018-04-01 11:13:10 +0900

    Figure out how to simulate typing in tests

--- test/app/components/editor/coreStylingPlugin.test.tsx
+++ test/app/components/editor/coreStylingPlugin.test.tsx
@@ -1,32 +1,67 @@
-// import * as React from 'react';
-// import * as assert from 'assert';
-// import '../../../utils/enzymeConfig';
-// import { mount } from 'enzyme';
-// import { Editor } from '../../../../app/components/editor/Editor';
-// import { EditorState } from 'draft-js';
+import * as React from 'react';
+import * as assert from 'assert';
+import '../../../utils/enzymeConfig';
+import { mount, ReactWrapper } from 'enzyme';
+import { Editor, EditorProps } from '../../../../app/components/editor/Editor';
+import { EditorState } from 'draft-js';
+import { noop } from 'lodash';
 
-// class EditorWrapper extends React.Component<{}, { editor: EditorState }> {
-//   state = { editor: EditorState.createEmpty() };
-//   editorRef: Editor;
-//   render() {
-//     return (
-//       <Editor ref={x => this.editorRef = x!} noteId="0" editor={this.state.editor} updateEditor={(({ editorState }: { editorState: EditorState }) => (console.log('updating'), this.setState({ editor: editorState }))) as any} />
-//     )
-//   }
-// }
+type EditorProvider = {
+  wrapper: ReactWrapper<{}, { editor: EditorState }>;
+  wrapperInstance: EditorWrapper;
+  editor: ReactWrapper<EditorProps, {}>;
+  editorInstance: Editor;
+  getEditorState: () => EditorState;
+  typeText: (text: string) => void;
+};
 
-// const setup = () => {
-//   const wrapper = mount<{}, { editor: EditorState }>(<EditorWrapper />);
-//   (wrapper.instance() as EditorWrapper).editorRef.focus();
-//   return wrapper;
-// };
+const createTypeText = (wrapper: ReactWrapper<{}, {}>) => {
+  return async (text: string) => {
+    const contentEditable = wrapper.find('.public-DraftEditor-content');
+    text.split('').forEach(char => contentEditable.simulate('beforeInput', { data: char, preventDefault: noop }));
+    await new Promise(resolve => setImmediate(resolve));
+  };
+}
 
-// describe('coreStylingPlugin', () => {
-//   describe('steps', () => {
-//     test('removeInlineStyles', () => {
-//       // const editor = setup();
-//       // editor.find('.public-DraftEditor-content').simulate('input', { target: { innerText: 'a' } });
-//       // assert.equal(editor.state().editor.getCurrentContent().getPlainText(), 'a');
-//     });
-//   });
-// });
+class EditorWrapper extends React.Component<{}, { editor: EditorState }> {
+  state = { editor: EditorState.createEmpty() };
+  editorRef: Editor;
+
+  render() {
+    return (
+      <Editor
+        ref={x => this.editorRef = x!}
+        noteId="0" editor={this.state.editor}
+        updateEditor={(({ editorState }: { editorState: EditorState }) => this.setState({ editor: editorState })) as any}
+      />
+    );
+  }
+}
+
+const setup = (): EditorProvider => {
+  (global as any).getSelection = () => ({});
+  const wrapper = mount<{}, { editor: EditorState }>(<EditorWrapper />);
+  const wrapperInstance = wrapper.instance() as EditorWrapper;
+  const editor = wrapper.find(Editor);
+  const provider: EditorProvider = {
+    wrapper,
+    wrapperInstance,
+    editor,
+    editorInstance: wrapperInstance.editorRef,
+    typeText: createTypeText(wrapper),
+    getEditorState: () => wrapper.state().editor
+  };
+
+  provider.editorInstance.focus();
+  return provider;
+};
+
+describe('coreStylingPlugin', () => {
+  describe('steps', () => {
+    test('removeInlineStyles', async () => {
+      const provider = setup();
+      await provider.typeText('hello');
+      assert.equal(provider.getEditorState().getCurrentContent().getPlainText(), 'hello');
+    });
+  });
+});
