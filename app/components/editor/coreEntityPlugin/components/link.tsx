20f7390f8af0cc9992824fa2b9d5c7b5b5fba957
Andrew Branch <andrew@wheream.io>
2018-05-27 23:43:25 -0700

    Fix cmd+click link while collapsed

--- app/components/editor/coreEntityPlugin/components/link.tsx
+++ app/components/editor/coreEntityPlugin/components/link.tsx
@@ -2,8 +2,15 @@ import * as React from 'react';
 import { LinkData } from '../entities/link';
 import { DraftDecoratorComponentProps } from 'draft-js-plugins-editor';
 import { openExternalOnCmdModifier } from '../../../../utils/openExternal';
+import { isMacOS } from '../../../../utils/platform';
+
+const onMouseDown: React.MouseEventHandler<HTMLAnchorElement> = event => {
+  if (isMacOS && event.metaKey || !isMacOS && event.ctrlKey) {
+    event.preventDefault();
+  }
+}
 
 export const Link: React.SFC<DraftDecoratorComponentProps> = ({ children, contentState, entityKey }) => {
   const { href }: LinkData = contentState.getEntity(entityKey).getData();
-  return <a href={href} onClick={openExternalOnCmdModifier}>{children}</a>;
+  return <a href={href} onMouseDown={onMouseDown} onClick={openExternalOnCmdModifier}>{children}</a>;
 };
